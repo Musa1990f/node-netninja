424b258488eea232e9a2d643eb271ec2f1ba02ce
Lukenge Faridu Musa <95524786+Musa1990f@users.noreply.github.com>
2022-12-24 05:13:18 +0900

    creating and removing directories have been added

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// Exporting / Requiring modules
+
+// const counter = ((arr) => {
+//   return 'There are' + arr.length + 'elements'
+// }); 
+
+// const adder = ((a,b) => {
+//   return `The sum of 2 numbers is ${a+b}`
+// });
+// const pi = 3.142;
+
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;
+
+// Wwe can also use this method
+
+// module.exports.counter= ((arr) => {
+//   return 'There are' + arr.length + 'elements'
+// }); 
+
+// module.exports.adder = ((a,b) => {
+//   return `The sum of 2 numbers is ${a+b}`
+// })
+
+// module.exports.pi =3.142;
+
---
+++ stuff.js
@@ -0,0 +1,78 @@
+//  no 1 Requiring /importing from app.js
+ 
+//  const stuff = require('./stuff');
+
+// console.log(stuff.counter(['yuka', 'musa', 'heno']));
+// console.log(stuff.addder(5,6));
+// console.log(stuff.addder(stuff.pi,5)); 
+
+
+// no. 2 Node Event Emitter
+
+// const events = require('events');
+// const { EventEmitter } = require('stream');
+// const util = require('util'); 
+ 
+
+// const Person = function(name){
+//   this.name = name;
+// }
+
+// util.inherits(Person,EventEmitter);
+
+// const musa = new person('musa');
+// const mary = new person('mary');
+// const mark = new person('mark');
+
+// const people = [musa,mary,mark];
+
+// people.forEach(function(person) {
+//   person.on('speak', function(mssg) {
+//     console.log(person.name + 'said' + mssg);
+//   });
+// });
+// musa.emit('hey');
+
+
+
+
+// Event module
+
+// n0 .3 Reading and writing files
+
+//  const fs = require('fs');
+
+// fs.readFile('readMe.txt', 'utf18' ,function(err,data){
+//   fs.readFile('writeMe.txt',data);
+// }
+
+// );
+
+// N0 .4 Creating directories with sync
+
+const fs = require('fs');
+
+// fs.mkdirSync('apex');
+
+//  removing directories you just have to remove the sync key word from mkdirSync
+
+// no-1 creating directories with async
+
+// fs. mkdir(function() {
+//   fs.readFile('readMe.txt' ,'utf8', function(err,data) {
+//     fs.writeFile('./stuff/writeMe.txt');
+//   });
+
+// });
+
+//no-2  removing files/directories
+
+fs.unlink('writeMe.txt', function() {
+  fs.rmdir('stuff');
+})
+
+
+
+
+
+
